2feb12a6bfee67be7eb4485dc73f7e571d43c07d
manicdon7 <manicdon7@gmail.com>
2024-03-15 19:20:10 +0530

    admin

--- src/pages/Admin/Auth.jsx
+++ src/pages/Admin/Auth.jsx
@@ -18,7 +18,7 @@ const Auth = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://myportfolio-backend-theta.vercel.app/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
