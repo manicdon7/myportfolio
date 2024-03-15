90b03565ea85e07b74430cb3be62d906190ac961
manicdon7 <manicdon7@gmail.com>
2024-03-15 20:42:34 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,7 @@ const bcrypt = require('bcrypt');
 const app = express();
 const port = 5000;
 
-const corsOptions = {
-  origin: 'https://manikandan05.vercel.app',
-};
-
-app.use(cors(corsOptions));
+app.use(cors('https://myportfolio-backend-theta.vercel.app/'));
 app.use(bodyParser.json());
 
 const dburi = process.env.dbURI;
