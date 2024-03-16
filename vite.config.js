db2bd2472899093481745237f1badc53eda24d5d
manicdon7 <manicdon7@gmail.com>
2024-03-16 19:02:48 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-  // server: {
-  //   proxy: {
-  //     '/api': 'https://manikandan05-backend.vercel.app/',
-  //     secure: false,
-  //   },
-  // },
-  // build: {
-  //   outDir: './build',
-  //   emptyOutDir: false,
-  // }
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://manikandan05-backend.vercel.app/',
+      secure: false,
+    },
+  },
+  build: {
+    outDir: './build',
+    emptyOutDir: false,
+  }
 })
