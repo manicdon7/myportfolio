1efe0a629a4aab09914abc7cadc490edee5e0a12
manicdon7 <manicdon7@gmail.com>
2024-03-16 11:37:13 +0530

    server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': '',
+      secure: false,
+    },
+  },
   plugins: [react()],
+  build: {
+    outDir: './build',
+    emptyOutDir: false,// Specify the output directory
+}
 })
