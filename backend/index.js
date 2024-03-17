feb3ca6c467a79ddbae6687d75be56704101e9cf
manicdon7 <manicdon7@gmail.com>
2024-03-17 13:03:23 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -141,5 +141,5 @@ app.get('/api/certificates', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on ${port}`);
 });
